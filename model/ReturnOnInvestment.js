408721902a438cc99a779d0a5c0f6215ac46ed54
Wilson Su <wilson_su@trend.com.tw>
2018-07-23 18:57:31 +0800

    update model 'ReturnOnInvestment'

--- model/ReturnOnInvestment.js
+++ model/ReturnOnInvestment.js
@@ -1,6 +1,8 @@
 
 module.exports = class ReturnOnInvestment {
-  constructor(player, table) {
-
+  constructor(player, table, action) {
+    this.investment = player.bet + action.bet;
+    this.reward = table.bet - this.investment;
+    this.rate = Number((this.reward / this.investment).toFixed(2));
   }
 };
