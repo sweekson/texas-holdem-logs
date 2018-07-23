3aad5984dc47e573ca336755bac77e7fdcaa87ae
Wilson Su <wilson_su@trend.com.tw>
2018-07-23 18:53:08 +0800

    update Player model props

--- model/Player.js
+++ model/Player.js
@@ -5,12 +5,10 @@ module.exports = class Player {
     this.chips = data.chips;
     this.cards = data.cards;
     this.bet = data.roundBet;
-    this.status = {
-      folded: data.folded,
-      allin: data.allIn,
-      survive: data.isSurvive,
-      online: data.isOnline,
-      human: data.isHuman
-    };
+    this.folded = data.folded;
+    this.allin = data.allIn;
+    this.survive = data.isSurvive;
+    this.online = data.isOnline;
+    this.human = data.isHuman;
   }
 }
