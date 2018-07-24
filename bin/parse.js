09181b271fd9b81134f7f5d1298b2fdf2d56b5bc
Wilson Su <wilson_su@trend.com.tw>
2018-07-24 15:10:02 +0800

    make parse script be able to handle multiple sources

--- bin/parse.js
+++ bin/parse.js
@@ -1,15 +1,27 @@
 
 const path = require('path');
+const glob = require('glob');
 const grab = require('ps-grab');
 
 const LogParser = require('../model/LogParser');
 
-const parser = new LogParser({
+const source = grab('--source');
+const pattern = grab('--pattern');
+const regress = grab('--regress') !== null;
+const options = {
+  regress,
   cwd: path.join(__dirname, '..', 'data/syslogs'),
-  src: grab('--name'),
   dest: path.join(__dirname, '..', 'data/logs'),
   batch: 10,
   rotate: 20
-});
+};
+const parse = options => new LogParser(options).parse();
+const foreach = async files => {
+  for (const file of files) {
+    options.src = file;
+    await parse(options);
+    options.regress = false;
+  }
+};
 
-parser.parse();
+foreach(source ? [source] : glob.sync(pattern, { cwd: options.cwd }));
