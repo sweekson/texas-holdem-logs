d5799928d0806b303e4c0717eaecfd08d4684178
Wilson Su <wilson_su@trend.com.tw>
2018-07-26 20:25:19 +0800

    add 'DynamicRequire' module

---
+++ model/DynamicRequire.js
@@ -0,0 +1,18 @@
+
+const path = require('path');
+const glob = require('glob');
+
+const modules = {};
+
+module.exports = {
+  require(source, cwd) {
+    glob.sync(source, { cwd }).forEach(file => {
+      const suffix = path.extname(file);
+      const basename = path.basename(file, suffix);
+      modules[basename] = require(`${cwd}/${file}`);
+    });
+  },
+  create(module, ...args) {
+    return new modules[module](...args);
+  }
+};
