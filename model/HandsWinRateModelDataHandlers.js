2b88d8c24b9fe909c19c8dd864231498ca79b930
Wilson Su <wilson_su@trend.com.tw>
2018-07-26 21:04:19 +0800

    add 'HandsWinRateModelDataHandlers' model

---
+++ model/HandsWinRateModelDataHandlers.js
@@ -0,0 +1,23 @@
+
+const Pokereval = require('./Pokereval');
+
+module.exports = class HandsWinRateModelDataHandlers {
+  input(original, formatted) {
+    if (original.table.stage !== 'Deal') { return null; }
+    const { table, player } = original;
+    const rate = Pokereval.rate(...player.cards);
+    return [].concat(
+      table.players,
+      Number((rate).toFixed(2)),
+      player.chips,
+    );
+  }
+
+  output(original, formatted) {
+    if (!original.table.board.length) { return null; }
+    const { action } = formatted;
+    const outputs = [0, 0, 0, 0, 0, 0];
+    outputs.splice(action.type, 1, 1);
+    return outputs;
+  }
+}
