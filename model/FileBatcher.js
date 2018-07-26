bb0e8273f60771779ee4395d3fbfb0df2b6e83f5
Wilson Su <wilson_su@trend.com.tw>
2018-07-26 14:47:15 +0800

    add 'FileBatcher' mode

---
+++ model/FileBatcher.js
@@ -0,0 +1,54 @@
+
+const path = require('path');
+const glob = require('glob');
+
+const FileReader = require('./FileReader');
+const FileWriter = require('./FileWriter');
+
+module.exports = class FileBatcher {
+  constructor(options) {
+    this.options = options;
+    this.buffer = [];
+    this.state = {};
+  }
+
+  concat() {
+    const state = this.state;
+    const cwd = this.options.cwd;
+    const pattern = this.options.pattern;
+    const batch = this.options.batch;
+    const files = glob.sync(pattern, { cwd });
+    const last = files.length - 1;
+
+    this.reset();
+
+    files.forEach((file, index) => {
+      const filepath = path.join(cwd, file);
+      const list = FileReader.json(filepath);
+      this.buffer = this.buffer.concat(list);
+      !state.start && (state.start = index + 1);
+      ++state.done;
+      (state.done === batch || index === last) && (state.end = index + 1);
+      state.start && state.end && this.flush();
+    });
+  }
+
+  flush() {
+    const state = this.state;
+    const start = String(state.start).padStart(5, '0');
+    const end = String(state.end).padStart(5, '0');
+    const filename = `${start}-${end}.json`;
+    const dest = this.options.dest;
+    const filepath = path.join(dest, filename);
+    FileWriter.json(filepath, this.buffer, null, 0);
+    this.reset();
+    console.log(`File ${filename} created`);
+  }
+
+  reset() {
+    this.state.done = 0;
+    this.state.start = 0;
+    this.state.end = 0;
+    this.buffer.splice(0);
+  }
+}
